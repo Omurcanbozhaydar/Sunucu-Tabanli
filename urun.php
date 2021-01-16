38accc80b73bbbbf2a90334377d369d60954aa9b
Omurcanbozhaydar <67788688+Omurcanbozhaydar@users.noreply.github.com>
2021-01-16 21:55:25 +0300

    Update urun.php

--- urun.php
+++ urun.php
@@ -3,7 +3,7 @@ try{
   $host='localhost';
   $vtadi='sirket';
   $kullanici='root';
-  $sifre='12345678';
+  $sifre='';
   $vt=new PDO("mysql:host=$host;dbname=$vtadi;charset=UTF8","$kullanici",$sifre);
 }
 catch(PDOException $e){
